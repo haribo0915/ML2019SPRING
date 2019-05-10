300c927e46d3897e933cfa446b0a8b7609de263c
haribo0915 <43194965+haribo0915@users.noreply.github.com>
2019-05-10 18:17:20 +0800

    Delete hw6_test.sh

--- hw6/hw6_test.sh
+++
@@ -1,6 +0,0 @@
-wget -O model1.h5 https://www.dropbox.com/s/llmz7k2yvhu2azw/model1.h5?dl=1
-wget -O model2.h5 https://www.dropbox.com/s/9tsdc9j3q85odlw/model2.h5?dl=1
-wget -O model3.h5 https://www.dropbox.com/s/mygushzmasq60l6/model3.h5?dl=1
-wget -O model4.h5 https://www.dropbox.com/s/du0w7j937tozx0p/model4.h5?dl=1
-wget -O model5.h5 https://www.dropbox.com/s/ggdupm6a0hd16d4/model5.h5?dl=1
-python test.py $1 $2 $3
